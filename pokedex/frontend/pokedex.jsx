be1e3b586e3487a01f0bc9fe1f672a2116c4c995
michelleroos <hello.michelleroos@gmail.com>
2021-05-04 16:52:30 -0400

    created and tested api util and pokemon action

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { fetchAllPokemon } from './util/api_util'
+import { receiveAllPokemon } from './actions/pokemon_actions';
+// import { RECEIVE_ALL_POKEMON } from './actions/pokemon_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     // const store = configureStore();
-    ReactDOM.render(<h1>Placeholder</h1>, root)
+    ReactDOM.render(<h1>Placeholder</h1>, root);
+
+    window.receiveAllPokemon = receiveAllPokemon;
+    window.fetchAllPokemon = fetchAllPokemon;
 })
+
+// const getSuccess = pokemon => console.log(receiveAllPokemon(pokemon));
+// fetchAllPokemon().then(getSuccess);
