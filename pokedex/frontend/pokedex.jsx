08e1dfdf8b790980258c9f8eb24adb5e044b9e4f
michelleroos <hello.michelleroos@gmail.com>
2021-05-04 17:29:41 -0400

    worked on reducers

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 
 import { fetchAllPokemon } from './util/api_util'
 import { receiveAllPokemon } from './actions/pokemon_actions';
+import { configureStore } from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    // const store = configureStore();
+    const store = configureStore();
+
     ReactDOM.render(<h1>Placeholder</h1>, root);
 
     window.receiveAllPokemon = receiveAllPokemon;
     window.fetchAllPokemon = fetchAllPokemon;
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
 })
 
 // const getSuccess = pokemon => console.log(receiveAllPokemon(pokemon));
