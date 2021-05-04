beedd0d47734f6b1d6d62445d744ab39365c6938
michelleroos <hello.michelleroos@gmail.com>
2021-05-04 13:56:14 -0400

    finished views/api/guest/show.json

--- jbuilder/config/routes.rb
+++ jbuilder/config/routes.rb
@@ -1,3 +1,19 @@
-Rails.application.routes.draw do
+# == Route Map
+#
+#          Prefix Verb URI Pattern                           Controller#Action
+# api_guest_gifts GET  /api/guests/:guest_id/gifts(.:format) api/gifts#index {:format=>:json}
+#      api_guests GET  /api/guests(.:format)                 api/guests#index {:format=>:json}
+#       api_guest GET  /api/guests/:id(.:format)             api/guests#show {:format=>:json}
+#        api_gift GET  /api/gifts/:id(.:format)              api/gifts#show {:format=>:json}
+#     api_parties GET  /api/parties(.:format)                api/parties#index {:format=>:json}
+#       api_party GET  /api/parties/:id(.:format)            api/parties#show {:format=>:json}
 
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    resources :guests, only: [:index, :show] do
+      resources :gifts, only: [:index]
+    end
+    resources :gifts, only: [:show]
+    resources :parties, only: [:index, :show]
+  end
 end
